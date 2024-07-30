f615ba905981f32ef0db53ca9faf7aab473823ae
leonardo manfrim <leonardo.manfrim123@hotmail.com>
2024-07-29 21:39:14 -0300

    Initial Commit

---
+++ projeto github sem terminal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projeto github sem terminal
+//
+//  Created by leonardo manfrim on 29/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Olá mundo")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
