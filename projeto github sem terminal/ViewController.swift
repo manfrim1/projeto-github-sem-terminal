6faa7a644adb69bdd1dd51f2d1509b0b4d87616c
leonardo manfrim <leonardo.manfrim123@hotmail.com>
2024-07-29 21:45:09 -0300

    Initial Commit

--- projeto github sem terminal/ViewController.swift
+++ projeto github sem terminal/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Olá mundo")
+        print("Olá Leonardo")
         // Do any additional setup after loading the view.
     }
 
